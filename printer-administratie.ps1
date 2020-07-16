b8bc37696aa411fafd73350bebf69fd251fc7f97
Ketelonderhoud <68386206+Ketelonderhoud@users.noreply.github.com>
2020-07-16 16:32:02 +0200

    Update printer-administratie.ps1

--- printer-administratie.ps1
+++ printer-administratie.ps1
@@ -4,7 +4,7 @@ if (!(Test-path -Path 'C:\temp\GEUPDPCL6Win_3500MU\driver\win_x64'))
 New-Item -ErrorAction Ignore -Path 'c:\temp' -ItemType Directory
 
 #Download driver from Develep
-Invoke-WebRequest -Uri "https://dl.develop.eu/nl/?tx_kmanacondaimport_downloadproxy[fileId]=3339bd9227b7b56e5c7b3b4fbef0d16f&tx_kmanacondaimport_downloadproxy[documentId]=102800&tx_kmanacondaimport_downloadproxy[system]=Develop&tx_kmanacondaimport_downloadproxy[language]=NL&type=1558521685" -OutFile "C:\GEUPDPCL6Win_3500MU.zip"
+Invoke-WebRequest -Uri "https://dl.develop.eu/nl/?tx_kmanacondaimport_downloadproxy[fileId]=3339bd9227b7b56e5c7b3b4fbef0d16f&tx_kmanacondaimport_downloadproxy[documentId]=102800&tx_kmanacondaimport_downloadproxy[system]=Develop&tx_kmanacondaimport_downloadproxy[language]=NL&type=1558521685" -OutFile "C:\temp\GEUPDPCL6Win_3500MU.zip"
 
 #Download 7zip
 #Invoke-WebRequest -Uri "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile "C:\temp\7z1900-x64.msi"
@@ -17,7 +17,8 @@ cmd /c "C:\Progra~1\7-Zip\7z.exe x c:\temp\GEUPDPCL6Win_3500MU.zip -oc:\temp\GEU
 Invoke-Command {pnputil.exe -a "C:\temp\GEUPDPCL6Win_3500MU\driver\win_x64\KOBS8JA_.inf" }
 Add-PrinterDriver -Name "Generic Universal PCL"
 Start-Sleep 3
-}
+}
+
 #Add printer port
 $portName = "10.10.100.94"
 $portExists = Get-Printerport -Name $portName -ErrorAction SilentlyContinue
